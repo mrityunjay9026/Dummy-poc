703b300a800120adc97583f1aca4c319d9d2c1c0
mrityunjay pandey <mrityunjaypandey9026@gmail.com>
2025-12-23 19:14:25 +0530

    Update: commit all changes

--- app/database.py
+++ app/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data/app.db"
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
